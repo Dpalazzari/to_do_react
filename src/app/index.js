a2a75f7c583744c541c0efae0e9e3a7681aac368
Drew Palazzari <drewpalazzari@hotmail.com>
2017-03-26 20:26:41 -0600

    props

--- src/app/index.js
+++ src/app/index.js
@@ -5,10 +5,20 @@ var ReactDOM = require('react-dom');
 var TodoComponent = React.createClass({
   render: function(){
     return(
-      <h1>Hello, World!</h1>
-    );
+      <div>
+        <p><strong>Cheese name:</strong> {this.props.cheese.name}</p>
+        <p><strong>Cheese smell:</strong> {this.props.cheese.smell}</p>
+        <p><strong>Cheese price:</strong> ${this.props.cheese.price}</p>
+      </div>
+    )
   }
-});
+})
+
+var myCheese = {
+  name: 'Camembert',
+  smell: 'extreme pong',
+  price: '3.50'
+}
 
 // put component into html page
-ReactDOM.render(<TodoComponent/>, document.getElementById('todo-wrapper'));
+ReactDOM.render(<TodoComponent mssg="I like Pizza" cheese={myCheese}/>, document.getElementById('todo-wrapper'));
