1d9c50287afb91cbd1ae9d5986eea39915fdde01
Drew Palazzari <drewpalazzari@hotmail.com>
2017-03-26 20:39:19 -0600

    Learning about changing state...cool

--- src/app/index.js
+++ src/app/index.js
@@ -6,20 +6,27 @@ var TodoComponent = React.createClass({
 
   getInitialState: function(){
     return {
-      todos: ['wash up', 'eat pizza', 'practice React']
+      todos: ['wash up', 'eat pizza', 'practice React'],
+      age: 27
     }
   },
-
   render: function(){
+
+    var ager = setTimeout(function(){
+      this.setState({
+        age: 72
+      })
+    }.bind(this), 5000)
+
     return(
       <div id='todo-list'>
         <p>The busiest people have the most leisure...</p>
+        <p>{this.state.age}</p>
         <ul>
           <li>{this.state.todos[0]}</li>
           <li>{this.state.todos[1]}</li>
           <li>{this.state.todos[2]}</li>
         </ul>
-        <listComponent todos={this.state.todos}/>
       </div>
     )
   } // render
